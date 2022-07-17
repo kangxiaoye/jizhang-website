acdc2a21ba6c901493ba7b0eaa11ed75ab7414e9
kangxiaoye <2198695788@qq.com>
2022-07-17 16:10:44 +0800

    'deploy'

---
+++ precache-manifest.9f7f0c8faf6bbc9b5fa567c1728a16e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91811f9bc08e48b5bcdf",
+    "url": "/jizhang-website/css/app.16ab68f8.css"
+  },
+  {
+    "revision": "01397c798d6bb85b9aabd5c42a251e30",
+    "url": "/jizhang-website/index.html"
+  },
+  {
+    "revision": "91811f9bc08e48b5bcdf",
+    "url": "/jizhang-website/js/app.da978a8a.js"
+  },
+  {
+    "revision": "9d8e09216c89301531df",
+    "url": "/jizhang-website/js/chunk-vendors.fbc0500b.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/jizhang-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhang-website/robots.txt"
+  }
+]);
